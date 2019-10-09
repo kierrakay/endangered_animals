8747b39e9ac232afdac05696fda89c79c6d6f9ee
Kierra M <k.merriman32@hotmail.com>
2019-10-09 12:55:34 +0000

    endangered_animals.gemspec

--- lib/endangered_animals/animals.rb
+++ lib/endangered_animals/animals.rb
@@ -7,11 +7,11 @@ class EndangeredAnimals::Animals
       1. African Elephant - Scientific name: Loxodonta africana - Conservation status - Vulnerable
       2. African Wild Dog - Scientific name: Lycaon  pictus - Conservation Status - Endangered 
       DOC
-      animal_1 = self.new 
-      animal_name = "African Elephant"
-      animal_1.scientific_name = "Loxodonta africana"
-      animal_1.conservation_status = true 
-      animal_1.url = "https://www.worldwildlife.org/species/directory"
+      # animal_1 = self.new 
+      # animal_name = "African Elephant"
+      # animal_1.scientific_name = "Loxodonta africana"
+      # animal_1.conservation_status = true 
+      # animal_1.url = "https://www.worldwildlife.org/species/directory"
       
   end 
   
