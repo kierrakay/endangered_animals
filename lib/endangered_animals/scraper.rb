1d644f73b92054d83755008277fd9ffffdf5b7bb
Kierra M <k.merriman32@hotmail.com>
2019-10-10 08:14:23 +0000

    lib/endangered_animals/scraper.rb

--- lib/endangered_animals/scraper.rb
+++ lib/endangered_animals/scraper.rb
@@ -6,6 +6,19 @@ class Scraper
     name = .css(" ").text.strip 
     scientific_name = .css(" ").text.strip
     path = .css(" ").attribute("href").value
-    url = 
-    
+    url = "https://www.worldwildlife.org#{path}"
+    animal = Animal.new(name, scientific_name, conservation_status, url) unless Animal.all.include?(animal)
+  end
 end
+
+def self.scraper_animal_details
+  new_page_landing = Nokogiri::HTML(open("#{animal.url}"))
+  animal.description = new_page_landing.css("").value 
+  animal.habit = new_page_landing.css("").value 
+  animal.scientific_name = new_page_landing.css("").value 
+  animal.conservation_status = new_page_landing.css("").value 
+  
+end 
+end
+  
+  
