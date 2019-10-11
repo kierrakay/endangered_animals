0a3a9295202b55d8fb552928151052aa6702f4ea
Kierra M <k.merriman32@hotmail.com>
2019-10-11 17:05:07 +0000

    lib/endangered_animals/scraper.rb

--- lib/endangered_animals/scraper.rb
+++ lib/endangered_animals/scraper.rb
@@ -15,10 +15,9 @@ class EndangeredAnimals::Scraper
       animal.scientific_name = new_animal_landing.css("div.container") [2].text.strip.split(". ").join << "."
       animal.habitat = new_animal_landing.css("div.container") [6].text.strip.split(". ").join << "."
       animal.conservation_status = new_animal_landing.css("div.container") [0].text.strip.split(". ").join << "."
-      animal.description = new_animal_landing.css("div.wysiwyg.lead p").text # #[0..3] return any range of elements?
-     
-      
-      
+      animal.description = new_animal_landing.css("div.wysiwyg.lead p") [0,2].text.strip 
+      # animal.description = new_animal_landing.css("div.wysiwyg.lead p").first.text.strip #split(".").join  # #[0..3] return any range of elements? <-- gets first paragraph on page
+      animal.description = new_animal_landing.css("div.wysiwyg.lead p") [0,2].text.strip #gets first and second paragraph with [0,2]
     end 
       
   end
