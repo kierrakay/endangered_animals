6b3bd2088892f8eddc8981541a3e702fb837c8bb
Kierra M <k.merriman32@hotmail.com>
2019-10-13 01:31:34 +0000

    lib/endangered_animals/scraper.rb

--- lib/endangered_animals/scraper.rb
+++ lib/endangered_animals/scraper.rb
@@ -16,10 +16,16 @@ class EndangeredAnimals::Scraper
       list = new_animal_landing.css("ul.list-data.list-stats")
      # list = new_animal_landing.css("ul.list-data.list-stats.list-items")
       animal.scientific_name = data.css("em")[0].text.strip.split(". ").join << "." if data.length >= 3
-      animal.habitat = list.css( "li:contains('Habitats')").text.strip 
-       #animal.habitat = list.css("li:nth-of-type(7) > div.container").text.strip.split(". ").join << "."  if data.length >= 7
-      # code above kinda works still needs to specify habitat
-      # animal.habitat = list.css("div.container").last.text.strip.split(". ").join << "." if data.length >= 7
+  
+        #animal.habitat = list.css( "li:contains('Habitats')") might not need contains('Habitats')
+         animal.habitat = list.css( "li:contains('Habitats')")
+          animal.habitat.children.each { |c| c.remove if c.name == 'i' }
+          animal.habitat.children.each { |c| c.remove if c.name == 'strong' }
+          animal.habitat= animal.habitat.text.strip
+         
+      #animal.habitat = list.css( "li:contains('Habitats')").text.gsub("e"," ").gsub("Habitats"," ").strip
+      # - will take out all the e's code above. need it to take out
+      
       animal.conservation_status = data [0].text.strip.split(". ").join << "." if data.length >= 1
       animal.description = new_animal_landing.css("div.wysiwyg.lead p") [0,2].text.strip #gets first and second paragraph with [0,2]
     end 
@@ -31,16 +37,14 @@ class EndangeredAnimals::Scraper
     # animal.description = new_animal_landing.css("div.wysiwyg.lead p").first.text.strip #split(".").join  # #[0..3] return any range of elements? <-- gets first paragraph on page
     
  
-#4 only habitat and conservation status wrong.
-#23 habitat missing 
+#4 conservation status wrong.
 #26 broke 
-#28 habitat wrong shows weight 
-#29 habitat and status scientific name wrong. this doesnt have that info. should only display description
-#31 doesnt have scientific name so nothing should display
+#29 conservation status and scientific name wrong .. should only display description
+#26,29,31, 35 doesnt have scientific name so nothing should display
 #35 broke
-#36 wrong habitat shows weight 
-#48 habitat shows length which is last property 
-#50 habitat missing 
+ #animal.habitat = list.css("li:nth-of-type(7) > div.container").text.strip.split(". ").join << "."  if data.length >= 7
+      # code above kinda works still needs to specify habitat
+
     
 
     #how can i get it to not show list of animals in menu when n is selcted. have it only print that select prompt.
